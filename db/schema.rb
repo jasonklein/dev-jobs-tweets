cf171e465b1496f1359cc1ea6eed22ce7b5b0ca0
jasonklein <jasonklein@gmail.com>
2014-08-02 09:51:24 +0100

    Add devise to User model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140802083331) do
+ActiveRecord::Schema.define(:version => 20140802084109) do
 
   create_table "tweets", :force => true do |t|
     t.string   "tweet_id"
@@ -23,8 +23,21 @@ ActiveRecord::Schema.define(:version => 20140802083331) do
 
   create_table "users", :force => true do |t|
     t.string   "screenname"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0,  :null => false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
