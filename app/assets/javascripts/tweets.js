3429fc9db5283e443278df455bec575556ea13bc
jasonklein <jasonklein@gmail.com>
2014-08-05 00:46:04 +0100

    Add active-button class for filter buttons; add js to swap toggle active-button class.

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -10,6 +10,8 @@ DevJobsTweetsApp.filter_with_isotope = function() {
   $('#filter-buttons').on( 'click', 'button', function() {
     var filterValue = $(this).attr('data-filter');
     $container.isotope({ filter: filterValue });
+    $('#filter-buttons').find('.active-button').removeClass('active-button');
+    $(this).addClass('active-button');
   });
 
 };
