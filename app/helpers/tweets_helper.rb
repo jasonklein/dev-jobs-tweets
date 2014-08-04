89e5fce9af3a2d34a639f8587a0dbf5b4d4ef24a
jasonklein <jasonklein@gmail.com>
2014-08-04 19:03:49 +0100

    Add view helper to assign method for tweet hexagons to color code them; add tweet hexagon color variables and apply.

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -1,2 +1,16 @@
 module TweetsHelper
+  def tweet_hex_class(tweet)
+    text = tweet.text.downcase
+    if tweet.twitter_created_at > 5.hours.ago
+      "new_tweet"
+    elsif text.include?("jr.") || text.include?("junior")
+      "junior"
+    else
+      if tweet.by_friend == true
+        "by-friend-" + ["1", "2", "3"].sample
+      else
+        "by-stranger-" + ["1", "2", "3"].sample
+      end
+    end
+  end
 end
