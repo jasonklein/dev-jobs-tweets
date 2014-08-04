888e533dc08762a75b03b6bb024aa36580b9831b
jasonklein <jasonklein@gmail.com>
2014-08-04 19:11:18 +0100

    Prioritize classification of junior tweets over new tweets in class assignment helper.

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -1,10 +1,10 @@
 module TweetsHelper
   def tweet_hex_class(tweet)
     text = tweet.text.downcase
-    if tweet.twitter_created_at > 2.hours.ago
-      "new-tweet"
-    elsif text.include?("jr.") || text.include?("junior")
+    if text.include?("jr.") || text.include?("junior")
       "junior"
+    elsif tweet.twitter_created_at > 2.hours.ago
+      "new-tweet"
     else
       if tweet.by_friend == true
         "by-friend-" + ["1", "2", "3"].sample
