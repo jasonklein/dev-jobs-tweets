54f67c2fcc5dcc258adcf2598a238e8a6280d6e1
jasonklein <jasonklein@gmail.com>
2014-12-26 18:16:41 -0500

    Adjust carrierwave to send to a development bucket when in development.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,6 +5,6 @@ CarrierWave.configure do |config|
     :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],        # required
     :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = ENV['DEV_JOBS_TWEETS_BUCKET']               # required
+  config.fog_directory  = Rails.env == "development" ? "#{ENV['DEV_JOBS_TWEETS_BUCKET']}-#{Rails.env}" : ENV['DEV_JOBS_TWEETS_BUCKET']               # required
   config.fog_public     = true                               # optional, defaults to true
 end
