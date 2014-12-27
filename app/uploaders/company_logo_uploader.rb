e12af7f59c1c3d1f63ad57909b9536bada86a755
jasonklein <jasonklein@gmail.com>
2014-12-27 00:27:50 -0500

    Adjust company logo scaling.

--- app/uploaders/company_logo_uploader.rb
+++ app/uploaders/company_logo_uploader.rb
@@ -29,7 +29,7 @@ class CompanyLogoUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
-  process resize_to_fill: [120, 54]
+  process resize_to_fit: [120, 54]
   #
   # def scale(width, height)
   #   # do something
